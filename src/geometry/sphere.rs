33b3eb6c414a2c3ab925e1ffcd9c26ab5011e340
Vesa Vilhonen <vesa@vilhonen.com>
2019-09-09 16:03:17 +0300

    Clean up

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -20,30 +20,16 @@ impl Sphere {
     }
 
     pub fn collides(&self, other: &Self) -> bool {
-        //                panic!("{:?} {:?} {:?} {:?} length:{}", self.center, other.center, self.radius, other.radius, (self.center - other.center).length());
         (self.center - other.center).length() < self.radius + other.radius
     }
 }
 
 impl Hitable for Sphere {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        /*
-        (A + tB - C) . (A + tB - C) = r^2
-        (tB+A-C).(tB+A-C) = r^2
-        tB.(tB+A-C) + (A-C).(tB+A-C) = r^2
-        tB.tB + tB.(A-C) + tB.(A-C) + (A-C).(A-C) - r^2 = 0
-        t^2(B.B) + 2t(B.(A-C)) + (A-C).(A-C) - r^2 = 0
-        */
-
         let oc = *r.origin() - self.center;
 
-        // t^2(B.B)
         let a = r.direction().dot(r.direction());
-
-        // 2t(B.(A-C))
         let b = r.direction().dot(&oc);
-
-        // (A-C).(A-C) - r^2
         let c = oc.dot(&oc) - self.radius * self.radius;
 
         let discriminant = b * b - a * c;
